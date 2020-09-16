2741d5942d2123b2fc5659e4b67ba5ed76c6d2a3
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-16 04:53:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 function batteries (){
   let totalBatteries = batteryBatches.reduce((s , c ) => s+c , 0);
 
-  console.log(totalBatteries);
+  return totalBatteries
 }
 batteries();
